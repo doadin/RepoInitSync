60754555eb238b91cca5fced67bdfebfc1e7b18c
Doadin <tbkizle@gmail.com>
2013-08-29 23:48:08 -0400

    Update Branch

--- update.sh
+++ update.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Updating Sources"
-mkdir -p ~/CM10
-cd ~/CM10
+mkdir -p ~/CM10.2
+cd ~/CM10.2
 repo sync -j4
 echo "Done"
 echo ""
 echo ""
 echo "Updating local Manifest"
 cd ~/Downloads
-wget "https://github.com/doadin/Cyanogenmod-local_manifests/archive/cm-10.zip"
-unzip Cyanogenmod-local_manifests-cm-10.zip -d ~/CM10/.repo/
-rm Cyanogenmod-local_manifests-cm-10.zip
+wget "https://github.com/doadin/Cyanogenmod-local_manifests/archive/cm-10.2.zip"
+unzip Cyanogenmod-local_manifests-cm-10.2.zip -d ~/CM10.2/.repo/
+rm Cyanogenmod-local_manifests-cm-10.2.zip
 echo ""
 echo ""
 echo "Re-Syncing for Local Manifest Changes"
