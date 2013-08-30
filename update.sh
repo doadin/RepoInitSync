87bd7d07447b9d0c78814ff4b1a2adcc316888bc
Doadin <tbkizle@gmail.com>
2013-08-29 23:43:03 -0400

    Update location
    is that possible to use /.repo ?

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ echo ""
 echo "Updating local Manifest"
 cd ~/Downloads
 wget "https://github.com/doadin/Cyanogenmod-local_manifests/archive/cm-10.zip"
-unzip Cyanogenmod-local_manifests-cm-10.zip -d ~/CM10
+unzip Cyanogenmod-local_manifests-cm-10.zip -d ~/CM10/.repo/
 rm Cyanogenmod-local_manifests-cm-10.zip
 echo ""
 echo ""
